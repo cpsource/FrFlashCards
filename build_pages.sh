3f126e6c83499f8ca449fe2fccd562be1acf0138
Cal Page <page.cal@gmail.com>
2025-10-23 18:09:58 +0000

    more animals

--- build_pages.sh
+++ build_pages.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 # Auto-generated script to build web pages
-# Generated from 26 .mp3 files in lesanimaux
+# Generated from 53 .mp3 files in lesanimaux
 
 set -e  # Exit on error
 
-python tools/bldwebpage.py "lesanimaux/en-lapin.png" "lesanimaux/en-lapin.mp3" "en lapin" 1 26
-python tools/bldwebpage.py "lesanimaux/un-canard.png" "lesanimaux/un-canard.mp3" "un canard" 2 26
-python tools/bldwebpage.py "lesanimaux/un-canari.png" "lesanimaux/un-canari.mp3" "un canari" 3 26
-python tools/bldwebpage.py "lesanimaux/un-chat.png" "lesanimaux/un-chat.mp3" "un chat" 4 26
-python tools/bldwebpage.py "lesanimaux/un-cheval.png" "lesanimaux/un-cheval.mp3" "un cheval" 5 26
-python tools/bldwebpage.py "lesanimaux/un-chien.png" "lesanimaux/un-chien.mp3" "un chien" 6 26
-python tools/bldwebpage.py "lesanimaux/un-cochon-d'lnde.png" "lesanimaux/un-cochon-d'lnde.mp3" "un cochon d'lnde" 7 26
-python tools/bldwebpage.py "lesanimaux/un-cochon.png" "lesanimaux/un-cochon.mp3" "un cochon" 8 26
-python tools/bldwebpage.py "lesanimaux/un-coq.png" "lesanimaux/un-coq.mp3" "un coq" 9 26
-python tools/bldwebpage.py "lesanimaux/un-dindon.png" "lesanimaux/un-dindon.mp3" "un dindon" 10 26
-python tools/bldwebpage.py "lesanimaux/un-furet.png" "lesanimaux/un-furet.mp3" "un furet" 11 26
-python tools/bldwebpage.py "lesanimaux/un-hamster.png" "lesanimaux/un-hamster.mp3" "un hamster" 12 26
-python tools/bldwebpage.py "lesanimaux/un-mouton.png" "lesanimaux/un-mouton.mp3" "un mouton" 13 26
-python tools/bldwebpage.py "lesanimaux/un-oiseau.png" "lesanimaux/un-oiseau.mp3" "un oiseau" 14 26
-python tools/bldwebpage.py "lesanimaux/un-perroquet.png" "lesanimaux/un-perroquet.mp3" "un perroquet" 15 26
-python tools/bldwebpage.py "lesanimaux/un-poisson-rouge.png" "lesanimaux/un-poisson-rouge.mp3" "un poisson rouge" 16 26
-python tools/bldwebpage.py "lesanimaux/un-poussin.png" "lesanimaux/un-poussin.mp3" "un poussin" 17 26
-python tools/bldwebpage.py "lesanimaux/un-taureau.png" "lesanimaux/un-taureau.mp3" "un taureau" 18 26
-python tools/bldwebpage.py "lesanimaux/un-âne.png" "lesanimaux/un-âne.mp3" "un âne" 19 26
-python tools/bldwebpage.py "lesanimaux/une-brebis.png" "lesanimaux/une-brebis.mp3" "une brebis" 20 26
-python tools/bldwebpage.py "lesanimaux/une-chèvre.png" "lesanimaux/une-chèvre.mp3" "une chèvre" 21 26
-python tools/bldwebpage.py "lesanimaux/une-oie.png" "lesanimaux/une-oie.mp3" "une oie" 22 26
-python tools/bldwebpage.py "lesanimaux/une-perruche.png" "lesanimaux/une-perruche.mp3" "une perruche" 23 26
-python tools/bldwebpage.py "lesanimaux/une-poule.png" "lesanimaux/une-poule.mp3" "une poule" 24 26
-python tools/bldwebpage.py "lesanimaux/une-tortue.png" "lesanimaux/une-tortue.mp3" "une tortue" 25 26
-python tools/bldwebpage.py "lesanimaux/une-vache.png" "lesanimaux/une-vache.mp3" "une vache" 26 26
+python tools/bldwebpage.py "lesanimaux/en-lapin.png" "lesanimaux/en-lapin.mp3" "en lapin" 1 53
+python tools/bldwebpage.py "lesanimaux/un-canard.png" "lesanimaux/un-canard.mp3" "un canard" 2 53
+python tools/bldwebpage.py "lesanimaux/un-canari.png" "lesanimaux/un-canari.mp3" "un canari" 3 53
+python tools/bldwebpage.py "lesanimaux/un-chameau.png" "lesanimaux/un-chameau.mp3" "un chameau" 4 53
+python tools/bldwebpage.py "lesanimaux/un-chat.png" "lesanimaux/un-chat.mp3" "un chat" 5 53
+python tools/bldwebpage.py "lesanimaux/un-cheval.png" "lesanimaux/un-cheval.mp3" "un cheval" 6 53
+python tools/bldwebpage.py "lesanimaux/un-chien.png" "lesanimaux/un-chien.mp3" "un chien" 7 53
+python tools/bldwebpage.py "lesanimaux/un-cochon-d'lnde.png" "lesanimaux/un-cochon-d'lnde.mp3" "un cochon d'lnde" 8 53
+python tools/bldwebpage.py "lesanimaux/un-cochon.png" "lesanimaux/un-cochon.mp3" "un cochon" 9 53
+python tools/bldwebpage.py "lesanimaux/un-coq.png" "lesanimaux/un-coq.mp3" "un coq" 10 53
+python tools/bldwebpage.py "lesanimaux/un-crabe.png" "lesanimaux/un-crabe.mp3" "un crabe" 11 53
+python tools/bldwebpage.py "lesanimaux/un-crocodile.png" "lesanimaux/un-crocodile.mp3" "un crocodile" 12 53
+python tools/bldwebpage.py "lesanimaux/un-dauphin.png" "lesanimaux/un-dauphin.mp3" "un dauphin" 13 53
+python tools/bldwebpage.py "lesanimaux/un-dindon.png" "lesanimaux/un-dindon.mp3" "un dindon" 14 53
+python tools/bldwebpage.py "lesanimaux/un-furet.png" "lesanimaux/un-furet.mp3" "un furet" 15 53
+python tools/bldwebpage.py "lesanimaux/un-gorille.png" "lesanimaux/un-gorille.mp3" "un gorille" 16 53
+python tools/bldwebpage.py "lesanimaux/un-hamster.png" "lesanimaux/un-hamster.mp3" "un hamster" 17 53
+python tools/bldwebpage.py "lesanimaux/un-hippocampe.png" "lesanimaux/un-hippocampe.mp3" "un hippocampe" 18 53
+python tools/bldwebpage.py "lesanimaux/un-hippopotame.png" "lesanimaux/un-hippopotame.mp3" "un hippopotame" 19 53
+python tools/bldwebpage.py "lesanimaux/un-lion.png" "lesanimaux/un-lion.mp3" "un lion" 20 53
+python tools/bldwebpage.py "lesanimaux/un-léopard.png" "lesanimaux/un-léopard.mp3" "un léopard" 21 53
+python tools/bldwebpage.py "lesanimaux/un-mouton.png" "lesanimaux/un-mouton.mp3" "un mouton" 22 53
+python tools/bldwebpage.py "lesanimaux/un-oiseau.png" "lesanimaux/un-oiseau.mp3" "un oiseau" 23 53
+python tools/bldwebpage.py "lesanimaux/un-ours.png" "lesanimaux/un-ours.mp3" "un ours" 24 53
+python tools/bldwebpage.py "lesanimaux/un-panda.png" "lesanimaux/un-panda.mp3" "un panda" 25 53
+python tools/bldwebpage.py "lesanimaux/un-perroquet.png" "lesanimaux/un-perroquet.mp3" "un perroquet" 26 53
+python tools/bldwebpage.py "lesanimaux/un-phoque.png" "lesanimaux/un-phoque.mp3" "un phoque" 27 53
+python tools/bldwebpage.py "lesanimaux/un-poisson-rouge.png" "lesanimaux/un-poisson-rouge.mp3" "un poisson rouge" 28 53
+python tools/bldwebpage.py "lesanimaux/un-poisson.png" "lesanimaux/un-poisson.mp3" "un poisson" 29 53
+python tools/bldwebpage.py "lesanimaux/un-poussin.png" "lesanimaux/un-poussin.mp3" "un poussin" 30 53
+python tools/bldwebpage.py "lesanimaux/un-requin.png" "lesanimaux/un-requin.mp3" "un requin" 31 53
+python tools/bldwebpage.py "lesanimaux/un-rhinocéros.png" "lesanimaux/un-rhinocéros.mp3" "un rhinocéros" 32 53
+python tools/bldwebpage.py "lesanimaux/un-singe.png" "lesanimaux/un-singe.mp3" "un singe" 33 53
+python tools/bldwebpage.py "lesanimaux/un-taureau.png" "lesanimaux/un-taureau.mp3" "un taureau" 34 53
+python tools/bldwebpage.py "lesanimaux/un-tigre.png" "lesanimaux/un-tigre.mp3" "un tigre" 35 53
+python tools/bldwebpage.py "lesanimaux/un-zèbre.png" "lesanimaux/un-zèbre.mp3" "un zèbre" 36 53
+python tools/bldwebpage.py "lesanimaux/un-âne.png" "lesanimaux/un-âne.mp3" "un âne" 37 53
+python tools/bldwebpage.py "lesanimaux/un-éléphant.png" "lesanimaux/un-éléphant.mp3" "un éléphant" 38 53
+python tools/bldwebpage.py "lesanimaux/une-baleine.png" "lesanimaux/une-baleine.mp3" "une baleine" 39 53
+python tools/bldwebpage.py "lesanimaux/une-brebis.png" "lesanimaux/une-brebis.mp3" "une brebis" 40 53
+python tools/bldwebpage.py "lesanimaux/une-chèvre.png" "lesanimaux/une-chèvre.mp3" "une chèvre" 41 53
+python tools/bldwebpage.py "lesanimaux/une-girafe.png" "lesanimaux/une-girafe.mp3" "une girafe" 42 53
+python tools/bldwebpage.py "lesanimaux/une-méduse.png" "lesanimaux/une-méduse.mp3" "une méduse" 43 53
+python tools/bldwebpage.py "lesanimaux/une-oie.png" "lesanimaux/une-oie.mp3" "une oie" 44 53
+python tools/bldwebpage.py "lesanimaux/une-otarie.png" "lesanimaux/une-otarie.mp3" "une otarie" 45 53
+python tools/bldwebpage.py "lesanimaux/une-perruche.png" "lesanimaux/une-perruche.mp3" "une perruche" 46 53
+python tools/bldwebpage.py "lesanimaux/une-pieuvre.png" "lesanimaux/une-pieuvre.mp3" "une pieuvre" 47 53
+python tools/bldwebpage.py "lesanimaux/une-poule.png" "lesanimaux/une-poule.mp3" "une poule" 48 53
+python tools/bldwebpage.py "lesanimaux/une-raie.png" "lesanimaux/une-raie.mp3" "une raie" 49 53
+python tools/bldwebpage.py "lesanimaux/une-tortue-de-mer.png" "lesanimaux/une-tortue-de-mer.mp3" "une tortue de mer" 50 53
+python tools/bldwebpage.py "lesanimaux/une-tortue.png" "lesanimaux/une-tortue.mp3" "une tortue" 51 53
+python tools/bldwebpage.py "lesanimaux/une-vache.png" "lesanimaux/une-vache.mp3" "une vache" 52 53
+python tools/bldwebpage.py "lesanimaux/une-étoile-de-mer.png" "lesanimaux/une-étoile-de-mer.mp3" "une étoile de mer" 53 53
 
 echo 'All pages built successfully!'
