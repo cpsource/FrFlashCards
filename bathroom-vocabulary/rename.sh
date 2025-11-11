52d17ca28dc1fe2e72f4df7587f1ac4960f3cdde
Cal Page <page.cal@gmail.com>
2025-11-11 19:46:28 +0000

    do rename

---
+++ bathroom-vocabulary/rename.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+set -e
+
+# Auto-generated rename commands
+
+# bathtub -> la baignoire
+mv "bathtub.mp3" "la-baignoire.mp3"
+mv "bathtub.png" "la-baignoire.png"
+
+# shower -> la douche
+mv "shower.mp3" "la-douche.mp3"
+mv "shower.png" "la-douche.png"
+
+# showerhead -> le pommeau de douche
+mv "showerhead.mp3" "le-pommeau-de-douche.mp3"
+mv "showerhead.png" "le-pommeau-de-douche.png"
+
+# toilet -> les toilettes
+mv "toilet.mp3" "les-toilettes.mp3"
+mv "toilet.png" "les-toilettes.png"
+
+# sink -> le lavabo
+mv "sink.mp3" "le-lavabo.mp3"
+mv "sink.png" "le-lavabo.png"
+
+# faucet -> le robinet
+mv "faucet.mp3" "le-robinet.mp3"
+mv "faucet.png" "le-robinet.png"
+
+# mirror -> le miroir
+mv "mirror.mp3" "le-miroir.mp3"
+mv "mirror.png" "le-miroir.png"
+
+# toothbrush -> la brosse à dents
+mv "toothbrush.mp3" "la-brosse-à-dents.mp3"
+mv "toothbrush.png" "la-brosse-à-dents.png"
+
+# toothpaste -> le dentifrice
+mv "toothpaste.mp3" "le-dentifrice.mp3"
+mv "toothpaste.png" "le-dentifrice.png"
+
+# soap -> le savon
+mv "soap.mp3" "le-savon.mp3"
+mv "soap.png" "le-savon.png"
+
+# bar soap -> un savon en barre
+mv "bar_soap.mp3" "un-savon-en-barre.mp3"
+mv "bar_soap.png" "un-savon-en-barre.png"
+
+# liquid soap -> le savon liquide
+mv "liquid_soap.mp3" "le-savon-liquide.mp3"
+mv "liquid_soap.png" "le-savon-liquide.png"
+
+# shampoo -> le shampooing
+mv "shampoo.mp3" "le-shampooing.mp3"
+mv "shampoo.png" "le-shampooing.png"
+
+# conditioner -> l’après-shampooing
+mv "conditioner.mp3" "l-après-shampooing.mp3"
+mv "conditioner.png" "l-après-shampooing.png"
+
+# towel -> la serviette
+mv "towel.mp3" "la-serviette.mp3"
+mv "towel.png" "la-serviette.png"
+
+# bath towel -> la serviette de bain
+mv "bath_towel.mp3" "la-serviette-de-bain.mp3"
+mv "bath_towel.png" "la-serviette-de-bain.png"
+
+# hand towel -> la serviette à main
+mv "hand_towel.mp3" "la-serviette-à-main.mp3"
+mv "hand_towel.png" "la-serviette-à-main.png"
+
+# washcloth -> le gant de toilette
+mv "washcloth.mp3" "le-gant-de-toilette.mp3"
+mv "washcloth.png" "le-gant-de-toilette.png"
+
+# towel rack -> le porte-serviettes
+mv "towel_rack.mp3" "le-porte-serviettes.mp3"
+mv "towel_rack.png" "le-porte-serviettes.png"
+
+# toilet paper -> le papier toilette
+mv "toilet_paper.mp3" "le-papier-toilette.mp3"
+mv "toilet_paper.png" "le-papier-toilette.png"
+
+# toilet brush -> la brosse de toilette
+mv "toilet_brush.mp3" "la-brosse-de-toilette.mp3"
+mv "toilet_brush.png" "la-brosse-de-toilette.png"
+
+# bidet -> le bidet
+mv "bidet.mp3" "le-bidet.mp3"
+mv "bidet.png" "le-bidet.png"
+
+# soap dish -> le porte-savon
+mv "soap_dish.mp3" "le-porte-savon.mp3"
+mv "soap_dish.png" "le-porte-savon.png"
+
+# toothbrush holder -> le porte-brosse à dents
+mv "toothbrush_holder.mp3" "le-porte-brosse-à-dents.mp3"
+mv "toothbrush_holder.png" "le-porte-brosse-à-dents.png"
+
+# mirror cabinet -> l’armoire de toilette
+mv "mirror_cabinet.mp3" "l-armoire-de-toilette.mp3"
+mv "mirror_cabinet.png" "l-armoire-de-toilette.png"
+
+# bath mat -> le tapis de bain
+mv "bath_mat.mp3" "le-tapis-de-bain.mp3"
+mv "bath_mat.png" "le-tapis-de-bain.png"
+
+# razor -> le rasoir
+mv "razor.mp3" "le-rasoir.mp3"
+mv "razor.png" "le-rasoir.png"
+
+# shaving cream -> la crème à raser
+mv "shaving_cream.mp3" "la-crème-à-raser.mp3"
+mv "shaving_cream.png" "la-crème-à-raser.png"
+
+# hair dryer -> le sèche-cheveux
+mv "hair_dryer.mp3" "le-sèche-cheveux.mp3"
+mv "hair_dryer.png" "le-sèche-cheveux.png"
+
+# comb -> le peigne
+mv "comb.mp3" "le-peigne.mp3"
+mv "comb.png" "le-peigne.png"
+
+# brush -> la brosse
+mv "brush.mp3" "la-brosse.mp3"
+mv "brush.png" "la-brosse.png"
+
+# deodorant -> le déodorant
+mv "deodorant.mp3" "le-déodorant.mp3"
+mv "deodorant.png" "le-déodorant.png"
+
+# light switch -> l’interrupteur
+mv "light_switch.mp3" "l-interrupteur.mp3"
+mv "light_switch.png" "l-interrupteur.png"
+
+# electric outlet -> la prise électrique
+mv "electric_outlet.mp3" "la-prise-électrique.mp3"
+mv "electric_outlet.png" "la-prise-électrique.png"
+
+# trash can -> la poubelle
+mv "trash_can.mp3" "la-poubelle.mp3"
+mv "trash_can.png" "la-poubelle.png"
+
+# toilet seat -> l’abattant des toilettes
+mv "toilet_seat.mp3" "l-abattant-des-toilettes.mp3"
+mv "toilet_seat.png" "l-abattant-des-toilettes.png"
+
+# plunger -> la ventouse
+mv "plunger.mp3" "la-ventouse.mp3"
+mv "plunger.png" "la-ventouse.png"
+
+# scale -> la balance
+mv "scale.mp3" "la-balance.mp3"
+mv "scale.png" "la-balance.png"
+
+# robe -> le peignoir
+mv "robe.mp3" "le-peignoir.mp3"
+mv "robe.png" "le-peignoir.png"
+
+# slippers -> les pantoufles
+mv "slippers.mp3" "les-pantoufles.mp3"
+mv "slippers.png" "les-pantoufles.png"
